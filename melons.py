ac27dbe463e808c454c02d6685b62e6f852e5c79
Angela Castanieto <angela@Jeffs-Mac.(none)>
2015-04-14 00:12:42 -0700

    Improved dictionary layout

--- melons.py
+++ melons.py
@@ -1,36 +1,55 @@
+# melon_dictionaries = {
+#      "melon_names": {
+#         1: "Honeydew",
+#         2: "Crenshaw",
+#         3: "Crane",
+#         4: "Casaba",
+#         5: "Cantaloupe",
+#     },
+#
+#     "melon_prices": {
+#         1: 0.99,
+#         2: 2.00,
+#         3: 2.50,
+#         4: 2.50,
+#         5: 0.99,
+#     },
+#
+#     "melon_seedlessness": {
+#         1: True,
+#         2: False,
+#         3: False,
+#         4: False,
+#         5: False,
+#     }
+#     }
+
 melon_dictionaries = {
-    "melon_names": {
-        1: "Honeydew",
-        2: "Crenshaw",
-        3: "Crane",
-        4: "Casaba",
-        5: "Cantaloupe",
-    },
-
-    "melon_prices": {
-        1: 0.99,
-        2: 2.00,
-        3: 2.50,
-        4: 2.50,
-        5: 0.99,
-    },
-
-    "melon_seedlessness": {
-        1: True,
-        2: False,
-        3: False,
-        4: False,
-        5: False,
+    "cantelope": {
+        "price": 0.99,
+        "seeds": True   },
+    "crenshaw": {
+        "price": 2.00,
+        "seeds": False   }
     }
-}
 
-def add_to_melon_dictionary(melon_dictionary, melon_number, melon_value):
+
+def update_melon_dictionary(melon_dictionary, melon_number, melon_value):
+
     if melon_dictionary in melon_dictionaries:
-        melon_dictionaries[melon_dictionary[melon_number]] = melon_value
-    else:
-        melon_dictionaries[melon_dictionary] = {}
-        melon_dictionaries[melon_dictionary[melon_number]] = melon_value
-    print melon_dictionaries   
+        print "Adding %s: %s to existing dictionary %s" % (melon_number,
+            melon_value, melon_dictionary)
+        melon_dictionary = melon_dictionaries[melon_dictionary]
+        melon_dictionary[melon_number] = melon_value
 
-add_to_melon_dictionary("melon_names", 6, "Watermelon")
-add_to_melon_dictionary("melon_color", 1, "green")
+    else:
+        print "Creating new dictionary %s and adding %s: %s" % (melon_dictionary,
+            melon_number, melon_value)
+        melon_dictionaries[melon_dictionary] = {}
+        melon_dictionaries[melon_dictionary][melon_number] = melon_value
+
+    print melon_dictionaries
+
+
+update_melon_dictionary("cantelope", "color", "orange")
+update_melon_dictionary("watermelon", "price", 0.99)
