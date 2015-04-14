bb2ecd67c31bced23c27a6d5cc96b9b308d0d484
Angela Castanieto <angela@Jeffs-Mac.(none)>
2015-04-14 08:12:36 -0700

    Added print function to print melon information

--- melons.py
+++ melons.py
@@ -1,55 +1,28 @@
-# melon_dictionaries = {
-#      "melon_names": {
-#         1: "Honeydew",
-#         2: "Crenshaw",
-#         3: "Crane",
-#         4: "Casaba",
-#         5: "Cantaloupe",
-#     },
-#
-#     "melon_prices": {
-#         1: 0.99,
-#         2: 2.00,
-#         3: 2.50,
-#         4: 2.50,
-#         5: 0.99,
-#     },
-#
-#     "melon_seedlessness": {
-#         1: True,
-#         2: False,
-#         3: False,
-#         4: False,
-#         5: False,
-#     }
-#     }
+from melonsdicts import melon_dictionaries
 
-melon_dictionaries = {
-    "cantelope": {
-        "price": 0.99,
-        "seeds": True   },
-    "crenshaw": {
-        "price": 2.00,
-        "seeds": False   }
-    }
-
-
-def update_melon_dictionary(melon_dictionary, melon_number, melon_value):
+def update_melon_dictionary(melon_dictionary, melon_key, melon_value):
 
     if melon_dictionary in melon_dictionaries:
-        print "Adding %s: %s to existing dictionary %s" % (melon_number,
+        print "Adding %s: %s to existing dictionary %s" % (melon_key,
             melon_value, melon_dictionary)
         melon_dictionary = melon_dictionaries[melon_dictionary]
-        melon_dictionary[melon_number] = melon_value
+        melon_dictionary[melon_key] = melon_value
 
     else:
         print "Creating new dictionary %s and adding %s: %s" % (melon_dictionary,
-            melon_number, melon_value)
+            melon_key, melon_value)
         melon_dictionaries[melon_dictionary] = {}
-        melon_dictionaries[melon_dictionary][melon_number] = melon_value
+        melon_dictionaries[melon_dictionary][melon_key] = melon_value
 
-    print melon_dictionaries
+    return melon_dictionaries
 
+def print_melon_info(melon_dictionaries):
+    print "Updated melon information:"
+    for melon, info_dict in melon_dictionaries.items():
+        print "Melon type: %s" % melon
+        for info, values in info_dict.items():
+            print info, values
+        print "\n"
 
-update_melon_dictionary("cantelope", "color", "orange")
-update_melon_dictionary("watermelon", "price", 0.99)
+print_melon_info(update_melon_dictionary("Cantaloupe", "color", "orange"))
+print_melon_info(update_melon_dictionary("Watermelon", "price", 0.99))
